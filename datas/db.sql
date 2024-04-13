53547a54a46696483eff06ca349d0b08a2091854
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-04-13 07:57:31 +0200

    add users table

--- datas/db.sql
+++ datas/db.sql
@@ -10,3 +10,9 @@ CREATE TABLE `locations`(
     `long` Decimal(9, 6) NOT NULL,
     `lat` Decimal(8, 6) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` VARCHAR(50) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL UNIQUE,
+  `password` VARCHAR(255) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NOT NULL
+);
